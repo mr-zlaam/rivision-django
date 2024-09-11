77b363a4315000d93442d1e65ac2d66ce9ded72c
mr-zlaam <zlaam.dev@gmail.com>
2024-09-11 19:24:07 +0500

    update settings.py, urls.py and views.py

--- src/_core/urls.py
+++ src/_core/urls.py
@@ -18,4 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("test/", include("myapp.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("test/", include("myapp.urls")),
+    path("", include("web.urls")),
+]
