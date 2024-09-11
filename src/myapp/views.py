660b64c68f77845b4f7309ee6fc20251b1f75ef8
mr-zlaam <zlaam.dev@gmail.com>
2024-09-11 18:28:09 +0500

    "Updated index.html template, added form and textarea; modified urls.py to include new counter path; added counter view function in views.py"

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    context = {"name": "Zlaam", "age": 28, "role": "Developer"}
+    return render(request, "index.html", {"context": context})
+
+
+def counter(request):
+    words: str = request.GET["textarea"]
+    len_of_words: int = len(words)
+
+    return render(request, "counter.html", {"length": len_of_words})
